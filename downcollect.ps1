19dc275e52ffbacd4b83e41176aedfd153239996
Louisjreeves <79279019+Louisjreeves@users.noreply.github.com>
2023-07-24 00:20:48 -0500

    Add files via upload

---
+++ downcollect.ps1
@@ -0,0 +1,50 @@
+﻿ 
+
+#$cred = Get-Credential
+
+ 
+
+ 
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+ 
+
+
+$mydownloads = (New-Object -ComObject Shell.Application).NameSpace('shell:Downloads').Self.Path
+
+$MyTemp =(Get-Item $mydownloads).fullname
+
+$myuser= $env:USERNAME
+
+
+ 
+
+   $response = Invoke-WebRequest -Uri https://github.com/Louisjreeves/collectlogs/raw/7dc93217d0785e18bc54e8c6ec767d74cb3bf172/collectlogs.zip -OutFile $mytemp\collectlogs.zip
+
+ 
+
+
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12 ;Expand-Archive -Path $mytemp\collectlogs.zip -DestinationPath $mydownloads\collectlogs -Force
+
+ 
+
+
+ $activedirectory = "C:\Users\$myuser\downloads\collectlogs\"
+
+cd c:\
+
+set-location $activedirectory
+
+  .\collectlogs.ps1 
+
+ 
+
+
+
+$MyDownloads = (New-Object -ComObject Shell.Application).NameSpace('shell:Downloads').Self.Path
+$MyTemp = (Get-Item $mydownloads).FullName
+
+$MyUser = $env:USERNAME
+
+  
